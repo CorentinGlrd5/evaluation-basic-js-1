39b783eb9ca32ed94dd974201fe3141a3e2519ed
corentin <corentin.guillard@gmail.com>
2018-03-13 10:39:58 +0100

    Exercise 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,6 +1,12 @@
 var removeNegativeValue = function(values) {
-  // Your future job begins here ...
+  var array = [];
+  return array;
+
+function nombre (value) {
+  return value >= 0;
 }
+var filtre = [34, -5, 9, 18, 0, -7, 32].filter(nombre);
+};
 
 module.exports = {
   title: 'Exercise 1',
